1611781cdc290131f60a3ad3c58506453dc93312
Dilsha Jahan <ddisha172048@bscse.uiu.ac.bd>
2021-08-18 14:15:20 +0600

    index.php file updated

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <body>
+	<form method="post" action="process.php">
+		First name:<br>
+		<input type="text" name="first_name">
+		<br>
+		Last name:<br>
+		<input type="text" name="last_name">
+		<br>
+		City name:<br>
+		<input type="text" name="city_name">
+		<br>
+		Email Id:<br>
+		<input type="email" name="email">
+		<br><br>
+		<input type="submit" name="create" value="create">
+		<input type="button" onclick="myFunction()" value = "View Info">
+	</form>
+
+  </body>
+</html>
