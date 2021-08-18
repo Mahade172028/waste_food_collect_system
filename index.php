412d8e1bcb5904a29719169dd556980f308747ea
Dilsha Jahan <ddisha172048@bscse.uiu.ac.bd>
2021-08-18 14:16:47 +0600

    sql and php code added in index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+include_once 'dbconfig.php';
+if(isset($_POST['create']))
+{	 
+	 $first_name = $_POST['first_name'];
+	 $last_name = $_POST['last_name'];
+	 $city_name = $_POST['city_name'];
+	 $email = $_POST['email'];
+	 $sql = "INSERT INTO userprofile (first_name,last_name,city_name,email)
+	 VALUES ('$first_name','$last_name','$city_name','$email')";
+	 if (mysqli_query($conn, $sql)) {
+		echo "Your account created successfully !";
+	 } else {
+		echo "Error: " . $sql . "
+" . mysqli_error($conn);
+	 }
+	 mysqli_close($conn);
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <body>
@@ -17,6 +37,10 @@
 		<input type="submit" name="create" value="create">
 		<input type="button" onclick="myFunction()" value = "View Info">
 	</form>
-
+	<script>
+		function myFunction(){
+			window.location.href="profileinfo.php"; 
+		}
+	</script>
   </body>
 </html>
